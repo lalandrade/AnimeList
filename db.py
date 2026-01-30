1816b98bba57df5850315d5fddd896153c80f15b
Lavínia <164552084+lalandrade@users.noreply.github.com>
2026-01-30 20:59:37 -0300

    atualizei

--- db.py
+++ db.py
@@ -1,12 +1,9 @@
+import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
-import os
 
 def get_connection():
     return psycopg2.connect(
-        host=os.getenv("DB_HOST", "localhost"),
-        database=os.getenv("DB_NAME", "crud_db"),
-        user=os.getenv("DB_USER", "postgres"),
-        password=os.getenv("DB_PASSWORD", "admin"),
+        os.environ.get("DATABASE_URL"),
         cursor_factory=RealDictCursor
     )
